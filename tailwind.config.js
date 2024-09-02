20975dc877c37c8e3075e5712d90a68549ed47df
Riccardo Bellini <riccardo.bellini@rold.com>
2024-09-02 15:44:05 +0200

    Working solution commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ['"Young serif"'],
+        sans: ['Outfit']
+      },
+      colors: {
+        stone: {
+          100: 'hsl(30, 54%, 90%)',
+          150: 'hsl(30, 18%, 87%)',
+          600: 'hsl(30, 10%, 34%)',
+          900: 'hsl(24, 5%, 18%)'
+        },
+        brown: {
+          800: 'hsl(14, 45%, 36%)'
+        },
+        rose: {
+          800: 'hsl(332, 51%, 32%)',
+          50: 'hsl(330, 100%, 98%)'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
